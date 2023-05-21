b7cb4f785d31590a19f30f5bfea3094dc61aade8
fabio <fabiocarvajal10@gmail.com>
2023-05-21 17:35:14 -0600

    manifesto 7 points

--- src/views/Manifesto.tsx
+++ src/views/Manifesto.tsx
@@ -1,11 +1,54 @@
 import useResponsive from '../hooks/useResponsive'
 import WIP from '../components/WIP'
+import { Blockquote, Flex, List, Stack, Title } from '@mantine/core'
+import Content from '../content/manifesto'
+import { 
+  IconSquareRoundedNumber1Filled, 
+  IconSquareRoundedNumber2Filled, 
+  IconSquareRoundedNumber3Filled,
+  IconSquareRoundedNumber4Filled,
+  IconSquareRoundedNumber5Filled,
+  IconSquareRoundedNumber6Filled,
+  IconSquareRoundedNumber7Filled,
+} from '@tabler/icons-react'
+import ResponsiveParagraph from '../components/Responsive/Paragraph'
 
+const ICONS = [
+  IconSquareRoundedNumber1Filled,
+  IconSquareRoundedNumber2Filled,
+  IconSquareRoundedNumber3Filled,
+  IconSquareRoundedNumber4Filled,
+  IconSquareRoundedNumber5Filled,
+  IconSquareRoundedNumber6Filled,
+  IconSquareRoundedNumber7Filled,
+]
 
 function ManifestoView() {
-  const { isScreenSmallerThan } = useResponsive()
   return (
-    <WIP/>
+    <Stack>
+      <Title ta="center" order={2} color="dark">
+        {Content.mainTitle}
+      </Title>
+
+      <Flex w="100%">
+        <List
+        >
+          {Content.policies.map((policy, i) => {
+            const Icon = ICONS[i]
+            return (
+              <List.Item py="xs" fz={32} icon={<Icon style={{ color: "violet"}}/>}>
+                <Title order={4} color="dark">
+                  {policy.title}
+                </Title>
+                <ResponsiveParagraph px="sm">
+                  {policy.desc}
+                </ResponsiveParagraph>
+              </List.Item>
+            )
+          })}
+        </List>
+      </Flex>
+    </Stack>
   )
 }
 
